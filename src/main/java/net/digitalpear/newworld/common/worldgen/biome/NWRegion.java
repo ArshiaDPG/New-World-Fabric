73f60836fb9ee5f75d32163e6bd52f4408b3d8b6
DP <digitalpeargaming@gmail.com>
2022-07-24 21:07:52 -0700

    improved biome placement

--- src/main/java/net/digitalpear/newworld/common/worldgen/biome/NWRegion.java
+++ src/main/java/net/digitalpear/newworld/common/worldgen/biome/NWRegion.java
@@ -6,12 +6,14 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.biome.source.util.MultiNoiseUtil;
 import terrablender.api.Region;
 import terrablender.api.RegionType;
 
+import java.util.List;
 import java.util.function.Consumer;
+
+import static terrablender.api.ParameterUtils.*;
 
 public class NWRegion extends Region {
 
@@ -21,6 +23,17 @@ public class NWRegion extends Region {
 
     @Override
     public void addBiomes(Registry<Biome> registry, Consumer<Pair<MultiNoiseUtil.NoiseHypercube, RegistryKey<Biome>>> mapper) {
-            this.addBiomeSimilar(mapper, BiomeKeys.MEADOW, NWBiomes.WOODED_MEADOW_KEY);
+        this.addModifiedVanillaOverworldBiomes(mapper, builder -> {
+            List<MultiNoiseUtil.NoiseHypercube> meadowPoints = new ParameterPointListBuilder()
+                    .temperature(Temperature.ICY, Temperature.COOL, Temperature.NEUTRAL)
+                    .humidity(Humidity.ARID, Humidity.DRY, Humidity.NEUTRAL, Humidity.WET, Humidity.HUMID)
+                    .continentalness(Continentalness.span(Continentalness.COAST, Continentalness.FAR_INLAND), Continentalness.span(Continentalness.MID_INLAND, Continentalness.FAR_INLAND))
+                    .erosion(Erosion.EROSION_0, Erosion.EROSION_1)
+                    .depth(Depth.SURFACE, Depth.FLOOR)
+                    .weirdness(Weirdness.HIGH_SLICE_VARIANT_ASCENDING, Weirdness.PEAK_VARIANT, Weirdness.HIGH_SLICE_VARIANT_DESCENDING)
+                    .build();
+
+            meadowPoints.forEach(point -> builder.replaceBiome(point, NWBiomes.WOODED_MEADOW_KEY));
+        });
     }
 }
