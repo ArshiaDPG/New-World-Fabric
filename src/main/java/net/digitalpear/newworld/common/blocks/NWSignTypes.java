ce3cc5306e6de9b609f28355b670766eb10c57ac
DP <digitalpeargaming@gmail.com>
2022-08-16 17:25:02 -0700

    bunker is finished

--- src/main/java/net/digitalpear/newworld/common/blocks/NWSignTypes.java
+++ src/main/java/net/digitalpear/newworld/common/blocks/NWSignTypes.java
@@ -4,6 +4,5 @@ import net.digitalpear.newworld.mixin.SignTypeAccessor;
 import net.minecraft.util.SignType;
 
 public class NWSignTypes {
-    public static final SignType FIR =
-            SignTypeAccessor.registerNew(SignTypeAccessor.newSignType("fir"));
+    public static final SignType FIR = SignTypeAccessor.registerNew(SignTypeAccessor.newSignType("fir"));
 }
