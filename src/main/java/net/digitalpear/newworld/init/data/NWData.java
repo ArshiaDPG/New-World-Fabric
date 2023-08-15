b89617bad7f7e99959f5fd2ffdc16b44093afe59
DP <digitalpeargaming@gmail.com>
2023-08-15 09:39:12 -0700

    fixed the new translation not working + mattock piece rebalances

--- src/main/java/net/digitalpear/newworld/init/data/NWData.java
+++ src/main/java/net/digitalpear/newworld/init/data/NWData.java
@@ -13,6 +13,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTables;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
@@ -40,13 +41,13 @@ public class NWData {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
             if (id.equals(LootTables.ANCIENT_CITY_CHEST) || id.equals(LootTables.STRONGHOLD_CORRIDOR_CHEST)){
                 tableBuilder.pool(LootPool.builder()
-                        .with(ItemEntry.builder(NWItems.MATTOCK_CRAFTING_TEMPLATE_SHAFT)
-                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(-3.0f, 1.0f))).build()));
+                        .with(ItemEntry.builder(NWItems.MATTOCK_CRAFTING_TEMPLATE_SHAFT))
+                        .conditionally(RandomChanceLootCondition.builder(0.15f)));
             }
             else if (id.equals(LootTables.DESERT_PYRAMID_CHEST)){
                 tableBuilder.pool(LootPool.builder()
                         .with(ItemEntry.builder(NWItems.MATTOCK_CRAFTING_TEMPLATE_HEAD)
-                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(-3.0f, 1.0f))).build()));
+                                .build()).conditionally(RandomChanceLootCondition.builder(0.15f)));
             }
             else if (id.equals(LootTables.TRAIL_RUINS_RARE_ARCHAEOLOGY)){
                 tableBuilder.modifyPools(builder -> builder.with(ItemEntry.builder(NWItems.MATTOCK_CRAFTING_TEMPLATE_HEAD)));
