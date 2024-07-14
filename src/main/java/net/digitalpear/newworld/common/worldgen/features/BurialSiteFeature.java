0f581b86b690db66df99e01827676e3dc2687ea8
DP <digitalpeargaming@gmail.com>
2024-07-13 21:24:46 -0700

    Burial site changes

--- src/main/java/net/digitalpear/newworld/common/worldgen/features/BurialSiteFeature.java
+++ src/main/java/net/digitalpear/newworld/common/worldgen/features/BurialSiteFeature.java
@@ -6,17 +6,23 @@ import net.digitalpear.newworld.init.NWBlockEntityTypes;
 import net.digitalpear.newworld.init.NWBlocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.enums.SlabType;
 import net.minecraft.loot.LootTables;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.UndergroundConfiguredFeatures;
 import net.minecraft.world.gen.feature.util.FeatureContext;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class BurialSiteFeature extends Feature<DefaultFeatureConfig> {
 
@@ -28,6 +34,7 @@ public class BurialSiteFeature extends Feature<DefaultFeatureConfig> {
     public boolean generate(FeatureContext<DefaultFeatureConfig> context) {
         StructureWorldAccess world = context.getWorld();
         BlockPos origin = context.getOrigin().down();
+        Random random = context.getRandom();
 
         int width = world.getRandom().nextBetween(5, 10);
 
@@ -35,6 +42,8 @@ public class BurialSiteFeature extends Feature<DefaultFeatureConfig> {
         BlockState crackedBricks = Blocks.CRACKED_DEEPSLATE_BRICKS.getDefaultState();
         BlockState tiles = Blocks.DEEPSLATE_TILES.getDefaultState();
         BlockState crackedTiles = Blocks.CRACKED_DEEPSLATE_TILES.getDefaultState();
+
+        List<BlockPos> towerPositions = new ArrayList<>();
 
         for (BlockPos pos : BlockPos.iterate(origin.add(-width, 1, -width), origin.add(width, 5, width))) {
             if (world.getBlockState(pos).isSolidBlock(world, pos)) {
@@ -51,10 +60,10 @@ public class BurialSiteFeature extends Feature<DefaultFeatureConfig> {
         }
 
         for (int i = -width; i <= width; i += 3){
-            makeTower(world, origin.add(-width, 0, -i));
-            makeTower(world, origin.add(width, 0, i));
-            makeTower(world, origin.add(-i, 0, width));
-            makeTower(world, origin.add(i, 0, -width));
+            towerPositions.add(origin.add(-width, 0, -i));
+            towerPositions.add(origin.add(width, 0, i));
+            towerPositions.add(origin.add(i, 0, -width));
+            towerPositions.add(origin.add(-i, 0, width));
 
             if (Math.abs(i) < width){
                 for (int k = -width; k < width; k += 3) {
@@ -70,16 +79,28 @@ public class BurialSiteFeature extends Feature<DefaultFeatureConfig> {
         }
         placeBlock(world, origin.up(), NWBlocks.TOMBSTONE.getDefaultState());
 
+        towerPositions.forEach(currentPos -> {
+            if (random.nextFloat() < 0.4){
+                makeTower(world, currentPos);
+            }
+        });
+
         return true;
     }
 
     private static void makeTower(StructureWorldAccess world, BlockPos pos){
-        for (BlockPos currentPos : BlockPos.iterate(pos.up(), pos.up(4))){
-            placeBlock(world, currentPos, Blocks.POLISHED_DEEPSLATE_WALL.getDefaultState());
+        int length = world.getRandom().nextBetween(4, 7);
 
+        for (BlockPos currentPos : BlockPos.iterate(pos.up(), pos.up(length))){
+            placeBlock(world, currentPos, Blocks.POLISHED_DEEPSLATE_WALL.getDefaultState());
         }
-        placeBlock(world, pos.up(5), Blocks.REDSTONE_LAMP.getDefaultState());
-        placeBlock(world, pos.up(6), Blocks.SCULK_SENSOR.getDefaultState());
+        placeBlock(world, pos.up(length+1), Blocks.REDSTONE_LAMP.getDefaultState());
+        placeBlock(world, pos.up(length+2), Blocks.SCULK_SENSOR.getDefaultState().with(Properties.WATERLOGGED, true));
+
+        Direction.stream().filter(direction -> direction.getAxis() != Direction.Axis.Y).forEach(direction -> {
+            placeBlock(world, pos.up(length+2).add(direction.getVector()), Blocks.DEEPSLATE_BRICKS.getDefaultState());
+            placeBlock(world, pos.up(length+1).add(direction.getVector()), Blocks.DEEPSLATE_BRICK_SLAB.getDefaultState().with(SlabBlock.TYPE, SlabType.TOP));
+        });
     }
 
     private static void generateTombstone(StructureWorldAccess world, BlockPos pos) {
@@ -87,6 +108,7 @@ public class BurialSiteFeature extends Feature<DefaultFeatureConfig> {
         world.getBlockEntity(pos, NWBlockEntityTypes.TOMBSTONE).ifPresent((blockEntity) ->
                 blockEntity.setLootTable(LootTables.UNDERWATER_RUIN_SMALL_CHEST, pos.asLong()));
     }
+
     private static void generateTombstone(StructureWorldAccess world, BlockPos pos, float chance) {
         if (world.getRandom().nextFloat() < chance){
             generateTombstone(world, pos);
